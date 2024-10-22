424629fde106b5005be3092176c0a77f8d6b22cd
Mayur <50207473+Mayur399@users.noreply.github.com>
2024-10-22 21:34:12 +0530

    Create src/test/java/runner/TestRunner.java

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,15 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+  features = "src/test/java/feature",
+  glue = {"stepDefinition"}
+)
+
+public class TestRunner {
+
+}
